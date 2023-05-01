9df07ed81aa7bbd5ce2189cc994bd3dc8dc84486
SadikRa <sadikrahman494@gmail.com>
2023-05-01 20:37:57 +0600

    added first commit

---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <h1>hi my name is sadik</h1>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './component/Main/Main';
+import Login from './component/Login/Login';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
